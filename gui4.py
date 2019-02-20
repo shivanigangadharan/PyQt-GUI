6705c16b645bb2169de6c0f49c5b8385333e5624
Himani Negi <negi.himani11@gmail.com>
2019-02-20 18:09:02 +0530

    Add files via upload

---
+++ gui4.py
@@ -0,0 +1,17 @@
+mport cv2
+import os
+
+image_folder = 'images'
+video_name = 'video.avi'
+
+images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+video = cv2.VideoWriter(video_name,cv2.VideoWriter_fourcc(*'DIVX'), 1, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
